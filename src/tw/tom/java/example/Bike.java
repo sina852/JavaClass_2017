abf51a6d4e8f02387b10d2ff07726b04b06ee12e
Tom.Tsai <sina852@gmail.com>
2017-10-23 19:14:38 +0800

    Day4

--- src/tw/tom/java/example/Bike.java
+++ src/tw/tom/java/example/Bike.java
@@ -3,6 +3,17 @@ package tw.tom.java.example;
 public class Bike {
 	
 	double speed;
+	
+	void upSpeed()
+	{
+		speed=speed<1?1:speed*1.2;
+	};
+	
+	
+	
+	
+	
+	
 	public void msg (String S) 
 	{
 		
--- src/tw/tom/java/example/Day4_StartBike.java
+++ src/tw/tom/java/example/Day4_StartBike.java
@@ -5,9 +5,13 @@ public class Day4_StartBike {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Bike b1 = new Bike();
+		b1.upSpeed();
 		b1.msg(String.valueOf(b1.speed));
-		
-
+		b1.upSpeed();
+		b1.upSpeed();
+		b1.upSpeed();
+		b1.upSpeed();
+		b1.msg(String.valueOf(b1.speed));
 	}
 
 }
