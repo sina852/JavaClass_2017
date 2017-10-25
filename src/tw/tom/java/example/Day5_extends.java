dd6f420ee49fb2b29f7a269eb627eb20cda7ca91
Tom.Tsai <sina852@gmail.com>
2017-10-25 21:30:08 +0800

    Day5

--- src/tw/tom/java/example/Day5_extends.java
+++ src/tw/tom/java/example/Day5_extends.java
@@ -14,6 +14,14 @@ public class Day5_extends {
 }
 class Tom1
 {
+//	Tom1()
+//	{
+//		System.out.println("tom1建構式");
+//	}
+	Tom1(int a) //建構式 爸爸有傳參數 兒子就要傳，不然就是在建一個沒傳參數的
+	{
+		System.out.println("tom1建構式");
+	}
 	int a =10;
 	 void m1() 
 	{
@@ -22,6 +30,12 @@ class Tom1
 }
 class Tom2 extends Tom1
 {
+	//第一道敘述句隱含super();
+	Tom2()
+	{
+		super(2);
+		System.out.println("tom2建構式");
+	}
 	@Override
 		void m1() {
 			// TODO Auto-generated method stub
